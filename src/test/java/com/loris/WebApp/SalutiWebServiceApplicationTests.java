f1684edaf29bb9bb65813422179b3a061c64bebb
LorisMinetti <92784723+LorisMinetti@users.noreply.github.com>
2022-07-20 11:13:27 +0200

    First Commit

---
+++ src/test/java/com/loris/WebApp/SalutiWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loris.WebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalutiWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
